26a6d902af3ccfb0737320f1bd729c85398741d2
SyimykZhak <zhakshybaevv@gmail.com>
2023-01-09 15:19:56 +0600

    adding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Добро пожаловать в магазин ebooks.kg
+            </p>
+            <p class="subtitle">
+              Лучший книжный магазин во всем Кыргызстане...
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">последние книги</h2>
+      </div>
+
+      <ProductBox 
+                v-for="product in latestProducts"
+                v-bind:key="product.id"
+                v-bind:product="product" />
+    
+  </div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data(){
+    return {
+    latestProducts: []
+  }
+},
+  components:{
+    ProductBox
+
+  },
+  mounted(){
+    this.getLatestProducts()
+    document.title = 'Книги на ваш вкус'
+  },
+  methods:{
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+      await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      this.$store.commit('setIsLoading', false)
+    }
   }
 }
 </script>
+
+<style scoped>
+.image{
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
